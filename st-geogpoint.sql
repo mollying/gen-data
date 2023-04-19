246d90d9850ece71c1554e869826084a4ca5fce2
mollying <90715524+mollying@users.noreply.github.com>
2023-04-18 19:15:01 -0500

    Update st-geogpoint.sql

--- st-geogpoint.sql
+++ st-geogpoint.sql
@@ -1,7 +1,7 @@
 SELECT
   voterbase_id,
   ST_GEOGPOINT(vb_reg_longitude, vb_reg_latitude) AS voter_coord
-FROM `prod-generation-data-176bffe6.viewers_dataset.GA_targetsmart_voter_base'
+FROM `prod-generation-data-176bffe6.viewers_dataset.GA_targetsmart_voter_base`
 WHERE
   vb_voterbase_age <= 25.0 AND
   vb_tsmart_county_name = 'CLARKE';
